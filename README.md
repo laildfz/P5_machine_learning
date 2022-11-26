4a7d30b6b3aa856a2201c042288c99628e6a4283
Laïl Desfossez <laildfz@gmail.com>
2022-11-26 17:41:23 +0100

    [Base] Base of my work

---
+++ README.md
@@ -0,0 +1,2 @@
+# P5_machine_learning
+P5 de ma formation Ingénieur machine learning 
---
+++ src/sql/request.sql
@@ -0,0 +1,26 @@
+DECLARE
+@start_date DATE
+DECLARE
+@end_date DATE
+
+SET @start_date = '2019-01-01'
+SET @end_date   = '2022-11-01'
+
+SELECT p.Id,
+       p.CreationDate,
+       p.Title,
+       p.Body,
+       p.Tags,
+       p.ViewCount,
+       p.CommentCount,
+       p.AnswerCount,
+       p.Score
+FROM Posts as p
+         LEFT JOIN PostTypes as t ON p.PostTypeId = t.Id
+WHERE p.CreationDate between @start_date and @end_date
+  AND t.Name = 'Question'
+  AND p.ViewCount > 20
+  AND p.CommentCount > 5
+  AND p.AnswerCount > 0
+  AND p.Score > 5
+  AND LEN(Tags) - LEN(REPLACE(Tags, '<','')) > 0 
